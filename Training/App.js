e562a8775d8418bd83ac4aa4ae2f068f62f4f5df
VasudevAkhil <akhil.vasudev@kiwitech.com>
2019-09-22 13:48:53 +0530

    Flex box

--- Training/App.js
+++ Training/App.js
@@ -1,28 +1,64 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-        <View style={styles.box} />
-    </View>
-  );
-};
+import Toggle from './Toggle'
+
+export default class App extends Component {
+
+  state = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  render() {
+    const {flexDirection, alignItems, justifyContent} = this.state
+    const layoutStyle = {flexDirection, justifyContent, alignItems}
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+
+    return (
+      <View style={styles.container}>
+        <Toggle
+          label={'Primary axis (flexDirection)'}
+          value={flexDirection}
+          options={['row', 'column']}
+          onChange={(option) => this.setState({flexDirection: option})}
+        />
+        <Toggle
+          label={primaryAxis + ' distribution (justifyContent)'}
+          value={justifyContent}
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
+          onChange={(option) => this.setState({justifyContent: option})}
+        />
+        <Toggle
+          label={secondaryAxis + ' alignment (alignItems)'}
+          value={alignItems}
+          options={['flex-start', 'center', 'flex-end', 'stretch']}
+          onChange={(option) => this.setState({alignItems: option})}
+        />
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box} />
+          <View style={styles.box} />
+          <View style={styles.box} />
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
   },
   box: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'skyblue',
-    borderWidth: 2,
-    borderColor: 'steelblue',
-    borderRadius: 20,
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5,
   },
 })
-
-export default App;
