2463107943ac4f6fba6da0d50e2c3d836c5d8292
VasudevAkhil <akhil.vasudev@kiwitech.com>
2019-09-22 13:54:47 +0530

    text

--- Training/App.js
+++ Training/App.js
@@ -1,48 +1,11 @@
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
-
-import Toggle from './Toggle'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default class App extends Component {
-
-  state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state
-    const layoutStyle = {flexDirection, justifyContent, alignItems}
-
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
     return (
       <View style={styles.container}>
-        <Toggle
-          label={'Primary axis (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']}
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distribution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']}
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
+        <Text style={styles.text}>Hello!</Text>
       </View>
     )
   }
@@ -51,14 +14,13 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
+  text: {
+    backgroundColor: 'whitesmoke',
+    color: '#4A90E2',
+    fontSize: 24,
+    padding: 10,
   },
 })
