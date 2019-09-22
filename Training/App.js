b8e9e2bbcf197a65cf6b57aa9a5a6d555917eda1
VasudevAkhil <akhil.vasudev@kiwitech.com>
2019-09-22 14:17:36 +0530

    scroll view

--- Training/App.js
+++ Training/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
-import { Image, StyleSheet } from 'react-native'
+import { ScrollView, View, StyleSheet } from 'react-native'
 
 export default class App extends Component {
   render() {
     return (
-      <Image
-        style={styles.image}
-        source={{uri: 'http://www.reactnativeexpress.com/static/logo.png'}}
-      />
+      <ScrollView style={styles.container}>
+        <View style={styles.boxLarge} />
+        <ScrollView horizontal>
+          <View style={styles.boxSmall} />
+          <View style={styles.boxSmall} />
+          <View style={styles.boxSmall} />
+        </ScrollView>
+        <View style={styles.boxLarge} />
+        <View style={styles.boxSmall} />
+        <View style={styles.boxLarge} />
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  image: {
+  container: {
+    flex: 1,
+  },
+  boxSmall: {
     width: 200,
     height: 200,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: 'skyblue',
+  },
+  boxLarge: {
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: 'steelblue',
   },
 })
